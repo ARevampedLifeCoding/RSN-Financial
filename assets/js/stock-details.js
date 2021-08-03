37a357df325a42a2a276fb4c2409e4770a8b7f58
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-02 20:16:28 -0600

    Added HTML structure for stock-details page with ID's and identified them in the js file

--- assets/js/stock-details.js
+++ assets/js/stock-details.js
@@ -1,5 +1,15 @@
-console.log("connected to stock-detail.js");
 const stockReference = document.querySelector("#stock-reference")
+const lastPrice = document.querySelector("#last-price")
+const fiftyAverage = document.querySelector("#fifty-ave")
+const twoHundredAve = document.querySelector("#two-hun-ave")
+const priceChanges = document.querySelector("#price-changes")
+const dayHi = document.querySelector("#day-hi")
+const dayLo = document.querySelector("#day-lo")
+const yearHi = document.querySelector("#year-hi")
+const yearLo = document.querySelector("#year-lo")
+const eps = document.querySelector("#eps")
+const peRatio = document.querySelector("#pe")
+const marketCap = document.querySelector("#market-cap")
 
 const baseStockUrl = "https://financialmodelingprep.com/api/v3/"
 const financialModelAPIKey = "?apikey=6404b2cc55178671f57f48fc947b5f75"
@@ -13,7 +23,12 @@ function init(){
     .then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
-                renderData(data[0]);
+                if (data) {
+                    renderData(data[0]);
+                }
+                else {
+                    console.log("invalid data returned");
+                }
             });
         } else {
             console.log("Error" + response.statusText);
@@ -26,6 +41,10 @@ function init(){
 
 function renderData(data) {
     console.log(data);
+    stockReference.innerHTML = data.name + " / " + data.symbol;
+
+
+
 }
 
 init();
