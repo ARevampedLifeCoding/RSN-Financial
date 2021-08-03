321d8d01f8eafce626af6dc08fde89bd82f6ed9c
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-02 20:57:17 -0600

    removed some old comments that were no longer valid

--- assets/js/stock-details.js
+++ assets/js/stock-details.js
@@ -15,10 +15,8 @@ const baseStockUrl = "https://financialmodelingprep.com/api/v3/"
 const financialModelAPIKey = "?apikey=6404b2cc55178671f57f48fc947b5f75"
 
 function init(){
-//    let ticker = JSON.parse(localStorage.getItem("ticker"));
-//    let stockName = JSON.parse(localStorage.getItem("stockName")) - need to set item on main script as well
-//    stockReference.innerHTML = stockName + " / " + ticker;
-    fetch(baseStockUrl + "quote/" + "AAPL" + financialModelAPIKey)
+    let ticker = JSON.parse(localStorage.getItem("ticker"));
+    fetch(baseStockUrl + "quote/" + ticker + financialModelAPIKey)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
